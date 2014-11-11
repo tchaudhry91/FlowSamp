e4a865fed7f36f9604359ca9739a9317840a3287
ryu <ryu@ryu-vm.(none)>
2014-11-12 00:51:11 +0900

    Added AutoInstall Script

--- requirements_install_script.sh
+++ requirements_install_script.sh
@@ -1,4 +1,5 @@
 sudo sed -i "s/\mirrors.kernel.org/old-releases.ubuntu.com/g" /etc/apt/sources.list
 sudo sed -i -e 's/archive.ubuntu.com|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
 
+sudo apt-get update
 sudo apt-get install bwm-ng tcpreplay
